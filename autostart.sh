d93ed9c31a14797b971f046cb03fe35f2c0f0647
Maarifa Maarifa <maarifamaarifa20@gmail.com>
2022-12-31 21:04:03 +0300

    Change wallpaper

--- autostart.sh
+++ autostart.sh
@@ -10,7 +10,7 @@ killall lxqt-policykit-agent
 
 picom --config "$SCRIPTPATH"/picom.conf &> /dev/null & 
 
-feh --bg-scale "${SCRIPTPATH}/wallpapers/butterfly-man.png" &
+feh --bg-scale "${SCRIPTPATH}/wallpapers/rusty-rust.jpg" &
 
 dunst -conf "$SCRIPTPATH"/dunstrc &
 
